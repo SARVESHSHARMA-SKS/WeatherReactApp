3ec402645a73e39c8c7892f1dc4316334cbbdbaa
Sarvesh Sharma <sssarvesh1999@gmail.com>
2022-08-04 00:27:44 +0530

    added Debouncing and default data Search

--- src/components/WeatherDetail.jsx
+++ src/components/WeatherDetail.jsx
@@ -1,65 +1,63 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { useEffect } from 'react'
-import { GraphChart } from './GraphChart'
+import React from "react";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { GraphChart } from "./GraphChart";
 import { useState } from "react";
 
-import axios from 'axios';
+import axios from "axios";
 import "./Weather.css";
 
 export const WeatherDetail = () => {
-    const [oneData,setLineOneData] = useState();
-    const { data } = useSelector(state => state)
-    let id = data?.weather;
-        if(id){
-        id = id[0].icon
+  const [oneData, setLineOneData] = useState();
+  const { data } = useSelector((state) => state);
+  let id = data?.weather;
+  if (id) {
+    id = id[0].icon;
+  }
+
+  useEffect(() => {
+    if (data && data.coord) {
+      getdataofUser(data.coord.lat, data.coord.lon);
     }
-    
-      useEffect(()=>{
-        if(data&& data.coord){
-            getdataofUser(data.coord.lat,data.coord.lon)
-        }
-    },[data])
+  }, [data]);
 
-    const getdataofUser = async(lat,lon)=>{
-        var { data } = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,alerts&units=metric&appid=efcbea2f5bd52d87caa6da2566f468ad`);
-        console.log(data);
-        setLineOneData(data)
-    }
-    return (
+  const getdataofUser = async (lat, lon) => {
+    var { data } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,alerts&units=metric&appid=efcbea2f5bd52d87caa6da2566f468ad`
+    );
+    console.log(data);
+    setLineOneData(data);
+  };
+  return (
+    <div>
+      <div style={{ fontSize: "2vw", fontWeight: "1000" }}>
+        Location: {data?.name}
+      </div>
+      <div style={{ fontSize: "2vw", fontWeight: "1000" }}>
+        Temperature: {data?.main?.temp}
+      </div>
 
-        <div>
-            <div style={{fontSize: '2vw',fontWeight:"1000"}}>Location: {data?.name}</div>
-            <div style={{ fontSize: '2vw', fontWeight: "1000" }}>Temperature: {data?.main?.temp}</div>
-           
-            <div>
-                {console.log(oneData)}
-        
-                
-            </div>
-<div>
-                {oneData && <GraphChart data={oneData.hourly} />}
-                </div>
+      <div></div>
+      <div>{oneData && <GraphChart data={oneData.hourly} />}</div>
 
-            <div className="mainbox">
-                
-                    <div className="leftMainBox">
-                        <span >Pressure</span>
-                        <span >{oneData && oneData.current.pressure} hpa</span>
-                        </div>
-                    <div className="rightMainBox">
-                    <span >Humidity</span>
-                    <span >{oneData && oneData.current.humidity} hpa</span>
-                    <div>
-                     
-                        </div>
-                </div>
-              
-            </div>
-                        <div style={{marginLeft:"20px"}}>
-                            <img style={{Width:"100%"}} src="https://i.ibb.co/KX3jkQp/sunriseandset.png"  alt="sunriseandset" />
-                        </div>
-
-            </div>
-    )
-}
+      <div className="mainbox">
+        <div className="leftMainBox">
+          <span>Pressure</span>
+          <span>{oneData && oneData.current.pressure} hpa</span>
+        </div>
+        <div className="rightMainBox">
+          <span>Humidity</span>
+          <span>{oneData && oneData.current.humidity} hpa</span>
+          <div></div>
+        </div>
+      </div>
+      <div style={{ marginLeft: "20px" }}>
+        <img
+          style={{ Width: "100%" }}
+          src="https://i.ibb.co/KX3jkQp/sunriseandset.png"
+          alt="sunriseandset"
+        />
+      </div>
+    </div>
+  );
+};
--- src/components/WeatherSearch.jsx
+++ src/components/WeatherSearch.jsx
@@ -1,17 +1,44 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { getWeatherData } from '../Redux/action';
 import { WeatherDetail} from './WeatherDetail';
 import "./Weather.css";
+import cityArr from "./CityData"
 const WeatherSearch = () => {
     const dispatch = useDispatch();
-    const [query, setQuery] = useState("");
-    const [bool, showBool] = useState(false)
+    const [query, setQuery] = useState("Munger");
+    const [bool, setBool] = useState(false);
+
+    const [cityData, setCityData] = useState([]);
+    const [cityList, setCityList] = useState(false);
+
+    useEffect(() => {
+           dispatch(getWeatherData(query));
+        setBool(true)
+    }, []);
     const handleSearch = () => {
         dispatch(getWeatherData(query))
         setQuery("")
-        showBool(true)
+        setBool(true)
+        setCityList(false)
     }
+    const handleClick = (event) => {
+    setQuery(event);
+        setCityList(false);
+            getWeatherData(query);
+  }
+
+    const cityLocation = () => {
+    if (query.trim() === '') {
+      setCityData([])
+    }
+    else {
+      let data = cityArr.filter(item => !item.name.toLowerCase().indexOf(query.toLowerCase()))
+      let  city= data.map(item => item.name);
+      setCityData(city)
+    }
+  }
+
     return (
         <div className="first-main">
             <div >
@@ -20,7 +47,11 @@ const WeatherSearch = () => {
                 </i></span>
             <input className="boxInp" type="text"
                 placeholder='enter location'
-                onChange={e => setQuery(e.target.value)}
+                    onChange={(e) => { 
+                        setQuery(e.target.value)
+                        setCityList(true)
+                        cityLocation()
+                    }}
                     value={query} />
                 <span className="srch"  onClick={handleSearch}>
                     <i className="fa fa-search">
@@ -28,7 +59,11 @@ const WeatherSearch = () => {
                  
                 {bool && <WeatherDetail/>}
 
-                </div>
+            </div>
+            <div  style={{ display: cityList ? "block" : "none",margin:"20px" }}>{cityData?.map(i => <div key={i.id} onClick={() => handleClick(i)} className='boxInp'>{i}
+               </div>)}</div>
+
+
         </div>
     )
 }
