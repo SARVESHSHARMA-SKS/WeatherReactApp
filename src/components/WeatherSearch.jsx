0146ccef6fbaceb9082a1be1580f6bc48630cde0
Sarvesh Sharma <sssarvesh1999@gmail.com>
2022-08-05 13:58:24 +0530

    Reverted the previous commit  for deployment

--- src/components/WeatherSearch.jsx
+++ src/components/WeatherSearch.jsx
@@ -1,77 +1,76 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import axios from "axios";
-
-import { WeatherDetail} from './WeatherDetail';
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { getWeatherData } from "../Redux/action";
+import { WeatherDetail } from "./WeatherDetail";
 import "./Weather.css";
-import cityArr from "./CityData"
+import cityArr from "./CityData";
 const WeatherSearch = () => {
+  const dispatch = useDispatch();
+  const [query, setQuery] = useState("Munger");
+  const [bool, setBool] = useState(false);
 
-    const [query, setQuery] = useState("Munger");
-    const [bool, setBool] = useState(false);
+  const [cityData, setCityData] = useState([]);
+  const [cityList, setCityList] = useState(false);
 
-    const [cityData, setCityData] = useState([]);
-    const [cityList, setCityList] = useState(false);
-
-    const  getWeatherData = async (text) => {
-     try {
-         const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${text}&units=metric&appid=efcbea2f5bd52d87caa6da2566f468ad`);
-      setCityData(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-
- }
-    const handleSearch = () => {
-     
-        setQuery("")
-        setBool(true)
-        setCityList(false)
-         getWeatherData(query);
-    }
-    const handleClick = (event) => {
+  useEffect(() => {
+    dispatch(getWeatherData(query));
+    setBool(true);
+  }, [query, dispatch]);
+  const handleSearch = () => {
+    dispatch(getWeatherData(query));
+    setQuery("");
+    setBool(true);
+    setCityList(false);
+  };
+  const handleClick = (event) => {
     setQuery(event);
-        setCityList(false);
-            getWeatherData(query);
-  }
+    setCityList(false);
+    getWeatherData(query);
+  };
 
-    const cityLocation = () => {
-    if (query.trim() === '') {
-      setCityData([])
+  const cityLocation = () => {
+    if (query.trim() === "") {
+      setCityData([]);
+    } else {
+      let data = cityArr.filter(
+        (item) => !item.name.toLowerCase().indexOf(query.toLowerCase())
+      );
+      let city = data.map((item) => item.name);
+      setCityData(city);
     }
-    else {
-      let data = cityArr.filter(item => !item.name.toLowerCase().indexOf(query.toLowerCase()))
-      let  city= data.map(item => item.name);
-      setCityData(city)
-    }
-  }
+  };
 
-    return (
-        <div className="first-main">
-            <div >
-                <span className="map">
-                    <i className="fa fa-map-marker">
-                </i></span>
-            <input className="boxInp" type="text"
-                placeholder='enter location'
-                    onChange={(e) => { 
-                        setQuery(e.target.value)
-                        setCityList(true)
-                        cityLocation()
-                    }}
-                    value={query} />
-                <span className="srch"  onClick={handleSearch}>
-                    <i className="fa fa-search">
-                    </i></span>
-                 
-                {bool && <WeatherDetail/>}
+  return (
+    <div className="first-main">
+      <div>
+        <span className="map">
+          <i className="fa fa-map-marker"></i>
+        </span>
+        <input
+          className="boxInp"
+          type="text"
+          placeholder="enter location"
+          onChange={(e) => {
+            setQuery(e.target.value);
+            setCityList(true);
+            cityLocation();
+          }}
+          value={query}
+        />
+        <span className="srch" onClick={handleSearch}>
+          <i className="fa fa-search"></i>
+        </span>
 
-            </div>
-            <div  style={{ display: cityList ? "block" : "none",margin:"20px" }}>{cityData?.map(i => <div key={i.id} onClick={() => handleClick(i)} className='boxInp'>{i}
-               </div>)}</div>
-
-
-        </div>
-    )
-}
+        {bool && <WeatherDetail />}
+      </div>
+      <div style={{ display: cityList ? "block" : "none", margin: "20px" }}>
+        {cityData?.map((i) => (
+          <div key={i.id} onClick={() => handleClick(i)} className="boxInp">
+            {i}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 export default WeatherSearch;
